837251c802f1eec945ad9b942effe407850e9820
Marinko Radic <mradic2@pmfst.hr>
2025-12-08 10:37:48 +0100

    Updated

--- app/page.js
+++ app/page.js
@@ -1,21 +1,7 @@
-"use client";
-
-import { useState } from "react";
-import Sidebar from "./components/Sidebar";
-import TeamsPage from "./teams/page";
-import CompetitionsPage from "./competition/page";
-
 export default function HomePage() {
-  //const [active, setActive] = useState("competitions");
-
   return (
     <div className="flex min-h-screen">
-      {/* <Sidebar active={active} setActive={setActive} /> */}
-
-      <div className=" p-8">
-        {/* {active === "competitions" && <CompetitionsPage />}
-        {active === "teams" && <TeamsPage />} */}
-      </div>
+      <div className=" p-8"></div>
     </div>
   );
 }
